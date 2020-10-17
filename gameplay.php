7e20adf0c49e82b4dafaa8306aef0a9996d4e178
Melinda <melindamorang@gmail.com>
2020-10-17 15:48:15 -0700

    Show a wait message after hitting submit

--- gameplay.php
+++ gameplay.php
@@ -13,37 +13,40 @@ $gid = $_GET["gid"];
   </head>
   <body>
   <?php include("snippets/banner.html"); ?>
-		<h2 id="instructions">Write a word, phrase, or sentence.</h2>
-		<canvas id="displayZone"></canvas>
-		<canvas id="inputZone"></canvas>
-		<div id="controlSection">
-			<div id="canvasInputs" hidden>
+  		<p id="waitMessage" hidden>Please wait until the other players have finished the round.</p>
+		<div id="inputArea">
+			<h2 id="instructions">Write a word, phrase, or sentence.</h2>
+			<canvas id="displayZone"></canvas>
+			<canvas id="inputZone"></canvas>
+			<div id="controlSection">
+				<div id="canvasInputs" hidden>
+					<br>
+					<input type="radio" name="thickness" id="fine" onclick="changeThickness('Fine')" checked="checked"></input>
+					<label for="fine">Fine</label>
+
+					<input type="radio" name="thickness" id="mfine" onclick="changeThickness('Medium Fine')"></input>
+					<label for="mfine">Medium Fine</label>
+
+					<input type="radio" name="thickness" id="mthick" onclick="changeThickness('Medium Thick')"></input>
+					<label for="mthick">Medium Thick</label>
+
+					<input type="radio" name="thickness" id="thick" onclick="changeThickness('Thick')"></input>
+					<label for="thick">Thick</label>
+
+					<br>
+					<input type="radio" name="tool" id="draw" onclick="changeTool('#000000')" checked="checked"></input>
+					<label for="draw">Draw</label>
+
+					<input type="radio" name="tool" id="erase" onclick="changeTool('#FFFFFF')"></input>
+					<label for="erase">Erase</label>
+				</div>
+
 				<br>
-				<input type="radio" name="thickness" id="fine" onclick="changeThickness('Fine')" checked="checked"></input>
-				<label for="fine">Fine</label>
-
-				<input type="radio" name="thickness" id="mfine" onclick="changeThickness('Medium Fine')"></input>
-				<label for="mfine">Medium Fine</label>
-
-				<input type="radio" name="thickness" id="mthick" onclick="changeThickness('Medium Thick')"></input>
-				<label for="mthick">Medium Thick</label>
-
-				<input type="radio" name="thickness" id="thick" onclick="changeThickness('Thick')"></input>
-				<label for="thick">Thick</label>
-
-				<br>
-				<input type="radio" name="tool" id="draw" onclick="changeTool('#000000')" checked="checked"></input>
-				<label for="draw">Draw</label>
-
-				<input type="radio" name="tool" id="erase" onclick="changeTool('#FFFFFF')"></input>
-				<label for="erase">Erase</label>
+				<button onclick="clearCanvas()">Clear</button>
+				<button onclick="submit()">Submit</button>
 			</div>
-
-			<br>
-			<button onclick="clearCanvas()">Clear</button>
-			<button onclick="submit()">Submit</button>
+			<input type="number" id="gid" hidden <?php echo "value='".$gid."'"; ?>></p>
+			<script src="game.js"></script>
 		</div>
-		<input type="number" id="gid" hidden <?php echo "value='".$gid."'"; ?>></p>
-		<script src="game.js"></script>
   </body>
 </html>
