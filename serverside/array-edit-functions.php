bd2db5a2343bd9ca24b6f5e88ac19e8c5fb380fd
Melinda <melindamorang@gmail.com>
2020-10-25 11:41:25 -0700

    Fix endgame stack to show player's stack instead of submissions
    The majority of updates are in create-gameplay-tables.php where we now populate a new database column with the name of the player whose stack the current row belongs to.

--- serverside/array-edit-functions.php
+++ serverside/array-edit-functions.php
@@ -1,40 +1,15 @@
 <?php
-function rotateArray($arr,$number){
-	//If they ask for "rotate 0 times", just give them back the array
-	if($number == 0){
-		return $arr;
+
+// For a given index and array length, if the index is negative,
+// wrap around and find a valid positive index within the array length.
+// This is because php is dumb and can't do negative array indexing automatically.
+// Example: getValidIndex(-1, 3) returns index 2 (the last item in the array)
+// Note: Does not help if the $idx is > $arrayLen-1
+function getValidIndex($idx, $arrayLen) {
+	if ($idx < 0) {
+		// Call this function recursively in case the resulting value is still negative.
+		$idx = getValidIndex($arrayLen + $idx, $arrayLen);
 	}
-
-	$output = [];
-
-	//Iterate from the starting number to the end of the array
-	for($i = $number; $i < count($arr); $i++){
-		//"$array[] =" is equivalent to pushing a new value onto the array in php
-		$output[] = $arr[$i];
-	}
-
-	//Then Iterate from 0 back around to the starting number
-	for($i = 0; $i < $number; $i++){
-		$output[] = $arr[$i];
-	}
-
-	/*
-	Basically, starts at array[number], iterates to the end, loops to 0, then up to array[number]
-	ex. rotateArray([0,1,2,3,4,5,6],2) should start at 2, go to the end, which is 6, start at 0, then go back to 2, exclusive.
-
-	[0,1,2,3,4,5,6] => [2,3,4,5,6,0,1];
-	*/
-
-	return $output;
-
-}
-
-function reverseArray($arr){
-	$output = [];
-	//Iterate from the end of the array down to 0
-	for($i = count($arr)-1; $i >= 0; $i--){
-		$output[] = $arr[$i];
-	}
-	return $output;
+	return $idx;
 }
 ?>
