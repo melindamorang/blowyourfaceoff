24c5351dc12c20d61d4a5225d9f934aad0ac711b
Melinda <melindamorang@gmail.com>
2020-10-26 20:28:11 -0700

    Store refresh rates in single easy-to-find place
    This was a good idea in theory, but the lobby pulse refresh rate is set in the javascript...

---
+++ serverside/refresh-rates.php
@@ -0,0 +1,6 @@
+<?php
+// Frequency of checking server during gameplay to see if all players
+//have submitted something for the round.
+$gameplayRefresh = 5;
+// I wanted to define the lobby pulse frequency in here as well, but that's set in js.
+?>
