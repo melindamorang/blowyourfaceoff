d01ef58c2f80dfd4b74797c4a91433aa0d09b027
Melinda <melindamorang@gmail.com>
2020-11-01 09:01:05 -0800

    Clean up

--- lobby-pulse.js
+++ lobby-pulse.js
@@ -10,8 +10,6 @@ function isGameStarted() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			// Typical action to be performed when the document is ready:
-			//document.getElementById("demo").innerHTML = xhttp.responseText;
 			console.debug(xhttp.responseText);
 			if (xhttp.responseText === null) {
 				addError("Error getting game status from server.");
