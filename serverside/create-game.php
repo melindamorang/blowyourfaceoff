77dba521ca697fcf68015b78ab3e311a655a9dbd
Melinda <melindamorang@gmail.com>
2020-11-03 21:02:36 -0800

    Change case
    This probably won't matter

--- serverside/create-game.php
+++ serverside/create-game.php
@@ -7,17 +7,17 @@ $name = json_decode($request_body,true)["name"];
 $gid = uniqid();
 
 include("open-database-connection.php");
-// Be sure this ID isn't taken by searching the GameStatus table for it
+// Be sure this ID isn't taken by searching the gamestatus table for it
 $gidTaken = true;
 while ($gidTaken) {
-	$takenIDResult = mysqli_query($link, "SELECT GameID FROM GameStatus WHERE GameID='" . $gid . "'");
+	$takenIDResult = mysqli_query($link, "SELECT GameID FROM gamestatus WHERE GameID='" . $gid . "'");
 	if (mysqli_num_rows($takenIDResult) == 0) $gidTaken = false;
 	// In the unlikely even that it was already taken, try again
 	else $gid = uniqid();
 }
 
 //Add the new game to the gamestatus table
-mysqli_query($link,"INSERT INTO GameStatus VALUES('".$gid."',0)");
+mysqli_query($link,"INSERT INTO gamestatus VALUES('".$gid."',0)");
 
 //Add the host to the waiting players table
 $name = mysqli_real_escape_string($link, $name);
