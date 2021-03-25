02224ac61cda8f4cf4466a3d1771a3fcc97e3e3f
Melinda Morang <melindamorang@gmail.com>
2021-03-24 20:42:39 -0700

    Fix merge error

--- gameplay.php
+++ gameplay.php
@@ -101,17 +101,12 @@ include("serverside/get-time-limit.php");
 							<div class="controlSet eraser">
 							<input type="radio" name="tool" id="erase" onclick="changeTool('#FFFFFF')"></input>
 							<label for="erase"><i class="fas fa-eraser"></i> Erase</label>
-						</div>
+							</div>
 
-						<div class="undoredo">
+							<div class="undoredo">
 							<button href="" name="undo" id="undo" onclick="undoHistory.undo();">Undo</button>
 							<button href="" name="redo" id="redo" onclick="undoHistory.redo();">Redo</button>
-						</div>
-
-						<div class="controlSet eraseall">
-							<button href="" onclick="clearInput();"><i class="fas fa-trash-alt"></i> Erase All</button>
-						</div>
-					</div>
+							</div>
 
 							<div class="controlSet eraseall">
 								<button href="" onclick="clearInput();"><i class="fas fa-trash-alt"></i> Erase All</button>
