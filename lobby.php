9ea4fd70f00f313205ac3a52ee98deb7278fcafc
sgriffin231 <73414055+sgriffin231@users.noreply.github.com>
2021-04-07 20:56:25 -0700

    lobby update and renewed favicon files
    Fixed forgotten lobby css and rearranged slightly for layout. Updated favicon per a suggestion I got this morning to invert the colors and make it a darker orange.

--- lobby.php
+++ lobby.php
@@ -6,21 +6,22 @@ $isHost = htmlspecialchars($_GET["isHost"]);
 <?php $pagename = "Lobby" ?>
 <?php include("header.php"); ?>
 
-<body>
-	<?php include("snippets/banner.html"); ?>
-	<h2>Game ID: <span id="gid"><?php echo $gid; ?></span></h2>
-	<span id="isHost" hidden><?php echo $isHost; ?></span>
-	<div id="hostArea">
-		<p>Send the Game ID code to the other players. When everyone has joined, click the Start Game button.</p>
-		<button onclick='startGame()'>Start Game</button>
-		<p id="ErrorLine"></p>
+	<div class="main-content">
+		<h2>Game ID: <span id="gid"><?php echo $gid; ?></span></h2>
+		<span id="isHost" hidden><?php echo $isHost; ?></span>
+		<h3>Current players:</h3>
+		<div id="nameList"></div>
+		
+		<div id="hostArea">
+			<p>Send the Game ID code to the other players. When everyone has joined, click the Start Game button.</p>
+			<p id="ErrorLine"></p>
+			<button onclick='startGame()'>Start Game</button>
+		</div>
+		
+		<div id="playerArea">
+			<p>Wait for the other players to join and the host to start the game.</p>
+		</div>
+		<script src="js/lobby-pulse.js"></script>
 	</div>
-	<div id="playerArea">
-		<p>Wait for the other players to join and the host to start the game.</p>
-	</div>
-	<p id="ErrorLine"></p>
-	<h3>Current players:</h3>
-	<div id="nameList"></div>
-	<script src="js/lobby-pulse.js"></script>
 
 <?php include("footer.php"); ?>
