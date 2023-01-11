f5bd948b4b0a4594e11dc2b8a040a4b19a5521b1
Melinda Morang <melindamorang@gmail.com>
2023-01-11 07:08:46 -0800

    Add some comments

--- serverside/open-database-connection.php
+++ serverside/open-database-connection.php
@@ -1,5 +1,9 @@
 <?php
 
+// Forbid caching so the database queries don't inadvertently neglect
+// to check the database for updated values and return a 304 code.
+// Caching was causing the site to fail to populate the lobby or
+// trigger the rounds to move forward.
 header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache"); // HTTP/1.0
