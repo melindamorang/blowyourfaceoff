38afb8d131d045c13f881375ada21d9e2319ddb8
Melinda Morang <melindamorang@gmail.com>
2023-01-22 19:22:15 -0800

    Try another thing

--- searchText.php
+++ searchText.php
@@ -1,6 +1,9 @@
 <?php
+$request_body = file_get_contents('php://input');
 $searchTextDisplay = htmlspecialchars($_GET["searchText"]);
+include("serverside/search-stack-text.php");
 ?>
+
 
 <?php
 // Forbid caching so the database queries don't inadvertently neglect
@@ -19,7 +22,51 @@ $searchTextDisplay = htmlspecialchars($_GET["searchText"]);
 		<h2>Showing results for: <span id="searchText"><?php echo $searchTextDisplay; ?></span></h2>
 
 		<div id="searchResults">
-			<?php include("serverside/search-stack-text.php"); ?>
+
+			<?php 
+
+				if(mysqli_num_rows($result)==0){
+					echo "<p>No results found.<p>";
+				} else {
+
+					// For each result, construct a URL to its associated stack and create an li with the stack text
+					$liStrings = array();
+					$currentGameID = "";
+					$currentStackOwner = "";
+					while($row = mysqli_fetch_assoc($result)){
+						$data = htmlspecialchars($row["ImgRef"]);
+						$gid = htmlspecialchars($row["GameID"]);
+						$owner = htmlspecialchars($row["StackOwner"]);
+						if ($gid == $currentGameID && $owner == $currentStackOwner) {
+							// Don't print this line because it's from the same stack as the last retrieved result
+							continue;
+						}
+						if (str_starts_with($data, "data:image/png;base64")) {
+							// Weed out erroneous results from the text strings defining drawings
+							continue;
+						}
+						$url = "./endgame.php?gid=" . $gid . "&name=" . $owner;
+						$liStrings[] = '<li><a href="' . $url . '" target="_blank" rel="noopener">' . $data . ' (Game ID ' . $gid . ', ' . $owner . '\'s stack)</a></li>';
+						$currentGameID = $gid;
+						$currentStackOwner = $owner;
+					}
+
+					$numResults = count($liStrings);
+					if ($numResults == 1){
+						echo "<p>" . $numResults . " result found.<p>";
+					} else {
+						echo "<p>" . $numResults . " results found.<p>";
+					}
+
+					// Create a ul and print links to the stacks
+					echo "<ul>";
+					foreach ($liStrings as $li) {
+						echo $li;
+					}
+					echo "</ul>";
+				}
+			?>
+			<!-- <?php include("serverside/search-stack-text.php"); ?> -->
 		</div>
 
 		<ul class="endStackMenu">
