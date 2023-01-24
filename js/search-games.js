41f912da85346d0d45a133a076ca15fe520ac3b8
Melinda Morang <melindamorang@gmail.com>
2023-01-23 18:59:31 -0800

    A different way of requesting the game text search

---
+++ js/search-games.js
@@ -0,0 +1,31 @@
+// Functions for searching old games
+
+// When the page first loads, show and hide content depending on round
+document.addEventListener('DOMContentLoaded', function () {
+    // Retrieve page elements and store them as global variables
+    // (This is done by not declaring them as var. Javascript is sneaky.)
+    searchText = document.getElementById("searchTextValue").value;
+    console.debug(searchText);
+    searchResultsDisplay = document.getElementById("searchResults");
+
+    // Search the database using the search string entered by the user
+    searchByText();
+
+});
+
+// Search the game database by the designated text string and return html to insert
+function searchByText() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Display the results in the display area
+            searchResultsDisplay.innerHTML = xhttp.responseText;
+        }
+        else {
+            searchResultsDisplay.innerHTML = "<p>Error retrieving search results from game database.</p>";
+        }
+    };
+    xhttp.open("GET", "serverside/search-stack-text.php?searchText=" + searchText, true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send();
+};
