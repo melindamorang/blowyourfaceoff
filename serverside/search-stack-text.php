1ff85c50a66c347e941ef6c25899dec6c2593d89
Melinda Morang <melindamorang@gmail.com>
2023-01-24 20:16:15 -0800

    Try natural language search mode

--- serverside/search-stack-text.php
+++ serverside/search-stack-text.php
@@ -5,7 +5,9 @@ $searchText = mysqli_real_escape_string($link, $_GET["searchText"]);
 // Can't use DISTINCT because we only want it to apply to GameID/StackOwner and not ImgRef.
 // DISTINCT ON (GameID, StackOwner) is not supported on our flavor of SQL.
 // Just grab them all and deal with it in post-processing.
-$result = mysqli_query($link, "SELECT GameID, StackOwner, ImgRef FROM game_data WHERE ImgRef LIKE '%" . $searchText . "%' ORDER BY GameID, StackOwner");
+//$result = mysqli_query($link, "SELECT GameID, StackOwner, ImgRef FROM game_data WHERE ImgRef LIKE '%" . $searchText . "%' ORDER BY GameID, StackOwner");
+$result = mysqli_query($link, "SELECT GameID, StackOwner, ImgRef FROM game_data WHERE MATCH(ImgRef) AGAINST ('" . $searchText . "' IN NATURAL LANGUAGE MODE)");
+include("close-database-connection.php");
 
 if ($result) {
     if(mysqli_num_rows($result)==0){
@@ -52,7 +54,5 @@ if ($result) {
 else {
     echo "<p>Error querying database: " . mysqli_error($link) . "</p>";
 }
-
-include("close-database-connection.php");
 
 ?>
